5014aebb9c57b94757ea35a674b3232e1d055255
Batu Kızmazoğlu <batukl89@gmail.com>
2024-11-18 23:18:41 +0200

    Spectrogram and Oscillogram added succesfully

--- tabs/histogram_tab.py
+++ tabs/histogram_tab.py
@@ -1,9 +1,10 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog
 from PyQt5.QtCore import Qt
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
-
+import sounddevice as sd
+import wave
 from utils.styles import COLORS
 from tabs.components import StyledButton
 
@@ -11,6 +12,7 @@ class RecordingTab(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.audio_data = None  # Mikrofon veya dosya verileri için depolama
 
     def init_ui(self):
         layout = QVBoxLayout(self)
@@ -20,33 +22,58 @@ class RecordingTab(QWidget):
         controls_layout = QHBoxLayout()
         self.record_button = StyledButton("🎤 Kayıt Başlat", COLORS['success'])
         self.stop_button = StyledButton("⏹ Kayıt Durdur", COLORS['error'])
+        self.load_button = StyledButton("📂 Ses Yükle", COLORS['primary'])
         self.stop_button.setEnabled(False)
         controls_layout.addWidget(self.record_button)
         controls_layout.addWidget(self.stop_button)
+        controls_layout.addWidget(self.load_button)
         layout.addLayout(controls_layout)
 
-        # Histogram için matplotlib figure
+        # Grafik için matplotlib figure
         plt.style.use('bmh')
-        self.figure, self.ax = plt.subplots(facecolor=COLORS['background'])
+        self.figure, (self.ax_oscillogram, self.ax_spectrogram) = plt.subplots(2, 1, figsize=(8, 6), facecolor=COLORS['background'])
         self.canvas = FigureCanvas(self.figure)
         layout.addWidget(self.canvas)
 
         # Sinyal bağlantıları
         self.record_button.clicked.connect(self.start_recording)
         self.stop_button.clicked.connect(self.stop_recording)
+        self.load_button.clicked.connect(self.load_audio_file)
 
     def start_recording(self):
         self.record_button.setEnabled(False)
         self.stop_button.setEnabled(True)
-        # Ses kaydı başlatma kodu
+        self.audio_data = []  # Yeni bir kayıt başlatıldığında sıfırlanır
+        self.stream = sd.InputStream(callback=self.audio_callback)
+        self.stream.start()
+
+    def audio_callback(self, indata, frames, time, status):
+        self.audio_data.extend(indata[:, 0])  # İlk kanalın verilerini sakla
 
     def stop_recording(self):
         self.record_button.setEnabled(True)
         self.stop_button.setEnabled(False)
-        #Ses kaydı durdurma ve analiz başlatma kodu
+        self.stream.stop()
+        self.stream.close()
+        self.plot_graphs(np.array(self.audio_data))
 
-    def update_histogram(self, audio_data):
-        self.ax.clear()
-        self.ax.hist(audio_data, bins=50, color=COLORS['primary'])
-        self.ax.set_facecolor(COLORS['background'])
+    def load_audio_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Ses Dosyası Yükle", "", "Ses Dosyaları (*.wav *.mp3)")
+        if file_path:
+            with wave.open(file_path, 'rb') as wf:
+                n_frames = wf.getnframes()
+                self.audio_data = np.frombuffer(wf.readframes(n_frames), dtype=np.int16)
+                self.plot_graphs(self.audio_data)
+
+    def plot_graphs(self, audio_data):
+        self.ax_oscillogram.clear()
+        self.ax_oscillogram.plot(audio_data, color=COLORS['primary'])
+        self.ax_oscillogram.set_title("Oscillogram", color=COLORS['text'])
+        self.ax_oscillogram.set_facecolor(COLORS['background'])
+
+        self.ax_spectrogram.clear()
+        self.ax_spectrogram.specgram(audio_data, Fs=44100, cmap='viridis')
+        self.ax_spectrogram.set_title("Spectrogram", color=COLORS['text'])
+        self.ax_spectrogram.set_facecolor(COLORS['background'])
+
         self.canvas.draw()
